8a8d8f9062980754c58dbbc6c6ebcbf8e40f45d9
Dolapo Adegoju <tichiegoju@gmail.com>
2022-12-25 22:53:00 +0000

    Done

--- day9/secret_auction.py
+++ day9/secret_auction.py
@@ -8,13 +8,14 @@ bidding_completed = False
 
 def max_bidder(bidding_record):
     max_bid = 0
+    winner = ""
 
     for bidder in (bidding_record):
         num_of_bid = bidding_record[bidder]
         if num_of_bid  > max_bid:          
             max_bid = num_of_bid   
             winner = bidder
-    print(f"The winner is {winner} with a bid of {max_bid}")
+    print(f"The winner is {winner} with a bid of ${max_bid}")
 
 while not bidding_completed:  
     name = input("What is your name?: ")   
