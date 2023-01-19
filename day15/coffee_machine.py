4a10f6259b212c93d0911b7e0260b41180bdd800
Dolapo <Tichiegoju@gmail.com>
2023-01-19 17:33:07 +0000

    Working on it

--- day15/coffee_machine.py
+++ day15/coffee_machine.py
@@ -37,7 +37,7 @@ recipes = {
         }
 }
 
-def avaliable_drinks(drinks):
+def prices(drinks):
     """Prints the price for each drink"""
     for drink in drinks:
         print(f"{drink}: ${drinks[drink]}")
@@ -57,24 +57,6 @@ def serving(drinks):
             elif customer_input == "off":
                 return "off"
 
-def check_resources(drinks, resources, customer_input, recipes):
-    """Checks the availability of resources for the customer's
-    request and returns True if enough resources
-    """
-    if customer_input in drinks:
-        for drink in recipes.keys():
-            if customer_input == drink:
-                for recipe, quantity in recipes[drink].items():
-                    if recipe == "water" and resources["Water"] > quantity:
-                        resources["Water"] -= quantity
-                    elif recipe == "milk" and resources["Milk"] > quantity:
-                        resources["Milk"] -= quantity
-                    elif recipe == "coffee" and resources["Coffee"] > quantity:
-                        resources["Coffee"] -= quantity
-                    else:
-                        print(f"Sorry there is not enough {recipe}.")
-                        break
-
 def resource_report(resources):
     """Prints the current resources and it's available values"""
     resource_water = resources["Water"]
@@ -84,55 +66,21 @@ def resource_report(resources):
     return(f"{resource_water}ml\n{resource_milk}ml\n{resource_coffee}g\n${resource_money}")
 
 
-def process_coin(amount):
+def process_coin(prices, customer_input):
     """Process user currency"""
     print("Please insert coins.")
-    total = int(input("how many quarters?: ")) * 0.25
-    total += int(input("how many dimes?: ")) * 0.1
-    total += int(input("how many nickles?: ")) * 0.05
-    total += int(input("how many pennies?: ")) * 0.01
-    return total
-
-
-def check_transaction(customer_input, total, drinks, resources):
-    """Checks if customer has inserted enough money for drink requested"""
-    if customer_input == "espresso" and total == drinks["espresso"]:
-        resources["Money"] += total
-        print(f"Here is your {customer_input}. Enjoy!")
-
-    elif customer_input == "espresso" and total > drinks["espresso"]:
-        customer_bal = total - drinks["espresso"]
-        resources["Money"] += drinks["espresso"]
-        print(f"${round(customer_bal, 2)} balance refunded, Thanks!")
-        print(f"Here is your {customer_input}. Enjoy!") 
-
-    elif customer_input == "latte" and total == drinks["latte"]:       
-        resources["Money"] += total                           
-        print(f"Here is your {customer_input}. Enjoy!") 
-
-    elif customer_input == "latte" and total > drinks["latte"]:   
-        customer_bal = total - drinks["latte"]  
-        resources["Money"] += drinks["latte"]   
-        print(f"${round(customer_bal, 2)} balance refunded, Thanks!")  
-        print(f"Here is your {customer_input}. Enjoy!")                                                                                     
-    elif customer_input == "cappuccino" and total == drinks["cappuccino"]:   
-        resources["Money"] += total        
-        print(f"Here is your {customer_input}. Enjoy!") 
-
-    elif customer_input == "cappuccino" and total > drinks["cappuccino"]:  
-        customer_bal = total - drinks["cappuccino"]    
-        resources["Money"] += drinks["cappuccino"]         
-        print(f"${round(customer_bal, 2)} balance refunded, Thanks!")             
-        print(f"Here is your {customer_input}. Enjoy!") 
-    else:
-        print("Sorry that's not enough money. Money refunded.")
+    total_money = int(input("how many quarters?: ")) * 0.25
+    total_money += int(input("how many dimes?: ")) * 0.1
+    total_money += int(input("how many nickles?: ")) * 0.05
+    total_money += int(input("how many pennies?: ")) * 0.01
+    return total_money
 
 
 continue_serving = True
 while continue_serving:
     print("Hi, Welcome to my coffee shop!")
     print()
-    avaliable_drinks(drinks)  
+    prices(drinks)  
     print()
 
     customer_input = serving(drinks)
@@ -144,10 +92,9 @@ while continue_serving:
         print(f"Resources Avaliable:\n{resource_report(resources)}")
         print()
     else:
-        amount = input("Enter a representation of coins amount (1 quarters = $0.25, 1 dimes = $0.10, 1 nickles = $0.05, 1 pennies = $0.01): ")
-        print()
-        total = process_coin(amount) 
-        print(f"Total money entered: ${total}\n") 
-        transaction = check_transaction(customer_input, total, drinks, resources)
+        sufficient_resource= check_resources(drinks, resources, customer_input, recipes)
+        total_money = process_coin(customer_input, prices) 
+        print(f"Total money entered: ${total_money}\n") 
+        transaction = check_transaction(customer_input, total_money, drinks, resources)
         print(f"{transaction}\n")
-        check_resources(drinks, resources, customer_input, recipes)
+
