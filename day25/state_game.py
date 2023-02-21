63b104e030ffa0e9a67c5da0102d36a4540cdc5f
Dolapo Adegoju <tichiegoju@gmail.com>
2023-02-21 14:11:33 +0100

    Fixed

--- day25/state_game.py
+++ day25/state_game.py
@@ -16,10 +16,7 @@ while len(guessed_state) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_state)}/50 states Correct", prompt="What's another state's name").title()
 
     if answer_state == "Exit":
-        missing_state = []
-        for state in all_state:
-            if state not in guessed_state:
-                missing_state.append(state)   
+        missing_state = [state for state in all_state if state not in guessed_state]
         new_data =pandas.DataFrame(missing_state)        
         new_data.to_csv("state_to_learn.csv")                
         break
